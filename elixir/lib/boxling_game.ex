82b1a70ee33433de08fb4d43fbff39d05147e97b
Michel Grootjans <michel.grootjans@gmail.com>
2016-03-06 21:39:02 +0100

    cleaner elixir solution

--- elixir/lib/boxling_game.ex
+++ elixir/lib/boxling_game.ex
@@ -1,21 +1,19 @@
 defmodule Game do
-	def score(rolls), do: score(1, rolls)
+  def score(rolls), do: score(1, rolls)
 
-	defp score(_frame, []), do: 0
+  #stop calculating frame score after frame 10
+  defp score(frame, _) when frame > 10, do: 0
 
-	#stop calculating after last frame
-	defp score(frame, _) when frame > 10, do: 0
+  #spare
+  defp score(frame, [roll1, roll2, roll3 | tail]) when roll1 + roll2 == 10 do
+    10 + roll3 + score(frame+1, [roll3|tail])
+  end
 
-	#strike
-	defp score(frame, [10, y, z | rolls]) do
-		10 + y + z + score(frame + 1, [ y, z | rolls])
-	end
+  #strike
+  defp score(frame, [10, roll2, roll3 | tail]) do
+    10 + roll2 + roll3 + score(frame+1, [roll2,roll3|tail])
+  end
 
-	#spare
-	defp score(frame, [x, y, z | rolls]) when x + y == 10 do
-		10 + z + score(frame + 1, [z | rolls])
-	end
-
-	defp score(_frame, [x]), do: x
-	defp score(_frame, [x, y | rolls]), do: x + y + score(rolls)
+  defp score(frame, [head|tail]), do: head + score(frame+1, tail)
+  defp score(_frame, []), do: 0
 end
--- elixir/test/bowling_game_test.exs
+++ elixir/test/bowling_game_test.exs
@@ -3,23 +3,30 @@ defmodule ElixirTest do
   doctest Game
 
   test "a new game", do: assert Game.score([]) == 0
+  test "rolling 0", do: assert Game.score([0]) == 0
   test "rolling 1", do: assert Game.score([1]) == 1
-  test "rolling 1,1", do: assert Game.score([1,1]) == 2
   test "rolling 2", do: assert Game.score([2]) == 2
+  test "rolling 1 twice", do: assert Game.score([1,1]) == 2
 
-  test "rolling spare-3" do
-    assert Game.score([4,6,3]) == 16
+  test "rolling a sequence" do
+    assert Game.score([1,2,3,4,5]) == (1+2+3+4+5)
   end
 
-  test "rolling strike-3-2" do
-    assert Game.score([10,10,3,2]) == (10+10+3) + (10+3+2) +(3+2)
+  test "rolling spare-3" do
+    assert Game.score([4,6,3]) == (4+6+3) + (3)
   end
 
   test "rolling all 5-es" do
-    assert Game.score([5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5]) == 150
+    rolls = Enum.take(Stream.cycle([5]), 21)
+    assert Game.score(rolls) == 150
+  end
+
+  test "rolling strike-2-3" do
+    assert Game.score([10,2,3]) == (10+2+3) + (2+3)
   end
 
   test "rolling a perfect game" do
-    assert Game.score([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]) == 300
+    rolls = Enum.take(Stream.cycle([10]), 12)
+    assert Game.score(rolls) == 300
   end
 end
