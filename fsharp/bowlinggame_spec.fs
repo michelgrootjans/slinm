d4843c5eaa78ec7e1756c874ce6f1190afd5a69a
Michel Grootjans <michel.grootjans@gmail.com>
2016-03-13 16:31:18 +0100

    starting in F#

--- fsharp/bowlinggame_spec.fs
+++ fsharp/bowlinggame_spec.fs
@@ -3,8 +3,14 @@ module BowlingGame =
 
   open Xunit
 
+  let rec score_for(rolls: int list) =
+    rolls.Head + score_for(rolls.Tail)
+
   let score(rolls: int list) =
-      0
+    match rolls with
+    | Null -> 0
+    | frame::rest
+      -> rolls.Head + score_for(rolls.Tail)
 
   type BowlingGameTests() =
     [<Fact>]
@@ -12,5 +18,17 @@ module BowlingGame =
         Assert.Equal(0,score [])
 
     [<Fact>]
-    member x.``rolling 0 scores 0``() =
+    member x.``rolling 0``() =
         Assert.Equal(0,score [0])
+
+    [<Fact>]
+    member x.``rolling 1``() =
+        Assert.Equal(1,score [1])
+
+      [<Fact>]
+      member x.``rolling 2``() =
+          Assert.Equal(2,score [2])
+
+      [<Fact>]
+      member x.``rolling 1 twice``() =
+          Assert.Equal(2,score [1;1])
