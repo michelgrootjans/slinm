4cc71a03902be67c1ec03adda3efca838cd4cefd
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-06 16:29:30 +0200

    fsharp: split test and implementation

---
+++ fsharp/bowlinggame.fs
@@ -0,0 +1,18 @@
+namespace BowlingGame
+module BowlingGame =
+
+  let first_roll_of rolls =
+    match rolls with
+      | head::_tail -> head
+      | _ -> 0
+
+  let rec score_for frame rolls =
+    match rolls with
+      | _ when frame > 10 -> 0
+      | roll1::roll2::tail when (roll1 + roll2 = 10) -> roll1 + roll2 + score_for(frame+1)(tail) + first_roll_of(tail)
+      | roll1::roll2::tail                           -> roll1 + roll2 + score_for(frame+1)(tail)
+      | roll::_tail -> roll
+      | _           -> 0
+
+  let rec score rolls =
+    score_for 1 rolls
--- fsharp/bowlinggame_spec.fs
+++ fsharp/bowlinggame_spec.fs
@@ -1,22 +1,7 @@
 namespace BowlingGame
-module BowlingGame =
 
   open Xunit
-  let first_roll_of rolls =
-    match rolls with
-      | head::tail -> head
-      | head -> 0
-
-  let rec score_for frame rolls =
-    match rolls with
-      | _ when frame > 10 -> 0
-      | roll1::roll2::tail when (roll1 + roll2 = 10) -> roll1 + roll2 + score_for(frame+1)(tail) + first_roll_of(tail)
-      | roll1::roll2::tail                           -> roll1 + roll2 + score_for(frame+1)(tail)
-      | roll::tail -> roll
-      | _          -> 0
-
-  let rec score rolls =
-    score_for 1 rolls
+  open BowlingGame
 
   type BowlingGameTests() =
     [<Fact>]
