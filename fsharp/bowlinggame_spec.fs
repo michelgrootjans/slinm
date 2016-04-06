8d403018d27d9779c343aef12ba2602fd31f626d
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-06 16:10:09 +0200

    fsharp: spares now work

--- fsharp/bowlinggame_spec.fs
+++ fsharp/bowlinggame_spec.fs
@@ -2,13 +2,21 @@ namespace BowlingGame
 module BowlingGame =
 
   open Xunit
+  let first_roll_of rolls =
+    match rolls with
+      | head::tail -> head
+      | head -> 0
+
+  let rec score_for frame rolls =
+    match rolls with
+      | _ when frame > 10 -> 0
+      | roll1::roll2::tail when (roll1 + roll2 = 10) -> roll1 + roll2 + score_for(frame+1)(tail) + first_roll_of(tail)
+      | roll1::roll2::tail                           -> roll1 + roll2 + score_for(frame+1)(tail)
+      | roll::tail -> roll
+      | _          -> 0
 
   let rec score rolls =
-    match rolls with
-    | x::y::rest -> x + y + score rest
-    | x::y       -> x + y
-    | [x]        -> x
-    | _          -> 0
+    score_for 1 rolls
 
   type BowlingGameTests() =
     [<Fact>]
@@ -28,5 +36,13 @@ module BowlingGame =
         Assert.Equal(2,score [1;1])
 
     [<Fact>]
-    member x.``rolling 1, 2, 3``() =
-        Assert.Equal(6,score [1;2;3])
+    member x.``rolling 2``() =
+        Assert.Equal(2,score [2])
+
+    [<Fact>]
+    member x.``rolling spare-2``() =
+        Assert.Equal(6+4+2 + 2,score [6;4;2])
+
+    [<Fact>]
+    member x.``rolling only fives``() =
+        Assert.Equal(150,score [5;5; 5;5; 5;5; 5;5; 5;5; 5;5; 5;5; 5;5; 5;5; 5;5; 5])
