55c1bfb889666605e4180746acea20142b95285e
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-06 10:45:46 +0200

    scala: spares now work

--- scala/src/main/scala/bowlinggame.scala
+++ scala/src/main/scala/bowlinggame.scala
@@ -1,9 +1,16 @@
 class BowlingGame {
    def score(rolls: List[Int]):Int = {
-     rolls match {
-       case Nil => 0
-       case roll1::roll2::tail if(roll1 + roll2 == 10) => 10 + first_roll_of(tail) + score(tail)
-       case head::tail => head + score(tail)
+     score(1, rolls)
+   }
+
+   def score(frame: Int, rolls: List[Int]):Int = {
+     frame match {
+       case 11 => 0
+       case _ => rolls match {
+         case Nil => 0
+         case roll1::roll2::tail if(roll1 + roll2 == 10) => 10 + first_roll_of(tail) + score(frame + 1, tail)
+         case head::tail => head + score(frame + 1, tail)
+       }
      }
    }
 
--- scala/src/test/scala/bowlinggame_test.scala
+++ scala/src/test/scala/bowlinggame_test.scala
@@ -7,27 +7,31 @@ class BowlingGameTest extends FunSuite with BeforeAndAfter {
     game = new BowlingGame
   }
 
-    test("a new game scores 0"){
-      assert(0 === game.score(List()))
-    }
+  test("a new game scores 0"){
+    assert(0 === game.score(List()))
+  }
 
-    test("rolling 0 scores 0"){
-      assert(0 === game.score(List(0)))
-    }
+  test("rolling 0 scores 0"){
+    assert(0 === game.score(List(0)))
+  }
 
-    test("rolling 1 scores 1"){
-      assert(1 === game.score(List(1)))
-    }
+  test("rolling 1 scores 1"){
+    assert(1 === game.score(List(1)))
+  }
 
-    test("rolling 2 scores 2"){
-      assert(2 === game.score(List(2)))
-    }
+  test("rolling 2 scores 2"){
+    assert(2 === game.score(List(2)))
+  }
 
-    test("rolling 1 twice scores 2"){
-      assert(2 === game.score(List(1,1)))
-    }
+  test("rolling 1 twice scores 2"){
+    assert(2 === game.score(List(1,1)))
+  }
 
-    test("rolling spare-3 scores 16"){
-      assert(16 === game.score(List(4,6,3)))
-    }
+  test("rolling spare-3 scores 16"){
+    assert((4+6+3 + 3) === game.score(List(4,6,3)))
+  }
+
+  test("rolling all 5-es scores 150"){
+    assert(150 === game.score(List.fill(21)(5)))
+  }
 }
