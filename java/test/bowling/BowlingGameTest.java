ad695244ffa67acb35e70557a5240b93979e8682
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-14 18:08:19 +0200

    next

--- java/test/bowling/BowlingGameTest.java
+++ java/test/bowling/BowlingGameTest.java
@@ -17,4 +17,10 @@ public class BowlingGameTest {
     public void ANewGameScores0() {
         assertEquals(0, game.getScore());
     }
+
+    @Test
+    public void Rolling_0() {
+        game.roll(0);
+        assertEquals(0, game.getScore());
+    }
 }
