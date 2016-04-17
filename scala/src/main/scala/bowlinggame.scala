963de484630d58eb25f5b66afbcca8bd4cdb924f
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-17 22:24:48 +0200

    roll 1 refactor

--- scala/src/main/scala/bowlinggame.scala
+++ scala/src/main/scala/bowlinggame.scala
@@ -2,8 +2,6 @@ class BowlingGame {
   def score(rolls: List[Int]):Int = {
     rolls match {
       case Nil => 0
-      case 0::tail => 0
-      case 1::tail => 1
       case head::tail => head
     }
   }
