fb696f45a893b87072227a2d4e465699665470d1
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-17 19:32:12 +0200

    next

--- javascript/tests/bowling-game-test.js
+++ javascript/tests/bowling-game-test.js
@@ -2,14 +2,15 @@ var chai = require('chai');
 var expect = chai.expect; // we are using the "expect" style of Chai
 var BowlingGame = require('./../src/bowling-game');
 
-describe('Bowling Game', function() {
-  it('a new game scores 0', function() {
-      var game = new BowlingGame();
-      expect(game.score()).to.equal(0);
-  });
-  it('rolling 0 scores 0', function() {
-      var game = new BowlingGame();
-      game.roll(0);
-      expect(game.score()).to.equal(0);
-  });
+describe('Bowling Game', function () {
+    var game;
+    beforeEach(function () { game = new BowlingGame(); });
+
+    it('a new game scores 0', function () {
+        expect(game.score()).to.equal(0);
+    });
+    it('rolling 0 scores 0', function () {
+        game.roll(0);
+        expect(game.score()).to.equal(0);
+    });
 });
