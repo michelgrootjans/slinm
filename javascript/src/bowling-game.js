6b6cd429265c94c3cf95e59d2b7b5bc9c0ad37e0
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-17 20:19:04 +0200

    next

--- javascript/src/bowling-game.js
+++ javascript/src/bowling-game.js
@@ -1,34 +1,40 @@
-var BowlingGame = function () {
-  var total = 0;
+var BowlingGame = function() {
   var rolls = [];
 
-  var roll  = function (pins) {
-    total += pins;
-    rolls.push(pins);
-  };
+  var roll = function(pins){ rolls.push(pins); };
 
-  function rollNumber(index) {
-    return rolls[index] || 0;
-  }
-
-  function firstRollOf(frame) {
-    return rollNumber(((frame - 1) * 2 + 1));
-  }
-
-  function secondRollOf(frame) {
-    return rollNumber(((frame - 1) * 2));
-  }
-
-  function scoreFor(frame) {
-    return secondRollOf(frame) + firstRollOf(frame);
-  }
-
-  var score = function () {
+  var score = function() {
     var score = 0;
-    for(var frame=1; frame <= 10; frame++) {
+    for (var frame = 1; frame <= 10; frame++){
       score += scoreFor(frame);
     }
     return score;
+  };
+
+  function baseScoreFor(frame) {
+    return firstRollOf(frame) + secondRollOf(frame);
+  }
+
+  var scoreFor = function(frame){
+    if( isSpare(frame))
+      return baseScoreFor(frame) + spareBonusFor(frame);
+    return baseScoreFor(frame);
+  };
+
+  var isSpare = function(frame){
+    return firstRollOf(frame) + secondRollOf(frame) == 10;
+  };
+
+  var spareBonusFor = function(frame){
+    return firstRollOf(frame + 1);
+  };
+
+  var firstRollOf = function(frame){
+    return rolls[(frame-1) * 2] || 0;
+  };
+
+  var secondRollOf = function(frame){
+    return rolls[(frame-1) * 2 + 1] || 0;
   };
 
   return { roll: roll, score: score };
