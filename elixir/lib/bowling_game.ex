e0dc8b10fcd06a586b39af9bb8185b52f2bcfdb6
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-17 20:43:55 +0200

    next

--- elixir/lib/bowling_game.ex
+++ elixir/lib/bowling_game.ex
@@ -1,9 +1,7 @@
 defmodule BowlingGame do
   def score([roll]), do: roll
 
-  def score([head|tail]) do
-    head + score(tail)
-  end
+  def score([head|tail]), do: head + score(tail)
 
   def score(_rolls) do
     0
