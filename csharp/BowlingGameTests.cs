dfe3de914f87f0e7df485a6f4b882a23dd903d0a
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-17 16:51:12 +0200

    next

--- csharp/BowlingGameTests.cs
+++ csharp/BowlingGameTests.cs
@@ -3,10 +3,15 @@ using Xunit;
 namespace BowlingGame
 {
   public class BowlingTest{
+    BowlingGame game;
+
+    public BowlingTest(){
+      game = new BowlingGame();
+    }
+
     [Fact]
-    public void ANewGameScores0()
-    {
-      Assert.Equal(0, new BowlingGame().Score);
+    public void ANewGameScores0(){
+      Assert.Equal(0, game.Score);
     }
   }
 }
