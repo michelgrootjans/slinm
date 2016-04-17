d2a655376585781e8793252537309cf237d2402d
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-17 20:54:13 +0200

    next

--- elixir/lib/bowling_game.ex
+++ elixir/lib/bowling_game.ex
@@ -1,7 +1,9 @@
 defmodule BowlingGame do
-  def score([roll1,roll2,roll3|tail]) when roll1 + roll2 == 10 do
-    roll1 + roll2 + roll3 + score([roll3|tail])
+  def score([roll1,roll2|tail]) when roll1 + roll2 == 10 do
+    roll1 + roll2 + first_roll_of(tail) + score(tail)
   end
   def score([head|tail]), do: head + score(tail)
   def score(_rolls), do: 0
+
+  def first_roll_of([head|tail]), do: head
 end
