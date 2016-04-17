694d4f997d9630f90e9fcb304690288944101909
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-17 20:45:21 +0200

    next

--- elixir/test/bowling_game_test.exs
+++ elixir/test/bowling_game_test.exs
@@ -18,7 +18,11 @@ defmodule ElixirTest do
     assert BowlingGame.score([2]) == 2
   end
 
-    test "rolling 1-1" do
-      assert BowlingGame.score([1,1]) == 2
-    end
+  test "rolling 1-1" do
+    assert BowlingGame.score([1,1]) == 2
+  end
+
+  test "rolling spare-2" do
+    assert BowlingGame.score([4,6,2]) == 4+6+2 + 2
+  end
 end
