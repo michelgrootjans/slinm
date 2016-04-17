64bea085864b3cc4b421db467a75c538de488275
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-17 20:10:11 +0200

    next

--- javascript/src/bowling-game.js
+++ javascript/src/bowling-game.js
@@ -1,8 +1,25 @@
 var BowlingGame = function () {
   var total = 0;
-  var roll  = function (pins) { total += pins; };
-  var score = function () { return total; };
+  var rolls = [];
+
+  var roll  = function (pins) {
+    total += pins;
+    rolls.push(pins);
+  };
+
+  function rollNumber(index) {
+    return rolls[index] || 0;
+  }
+
+  var score = function () {
+    var score = 0;
+    for(var frame=1; frame <= 10; frame++) {
+      score += rollNumber(((frame - 1) * 2)) + rollNumber(((frame - 1) * 2 + 1));
+    }
+    return score;
+  };
 
   return { roll: roll, score: score };
 };
+
 module.exports = BowlingGame;
