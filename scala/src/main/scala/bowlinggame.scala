1ec89081e2646bc681cc96725e4a9979180d7fb1
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-17 22:32:31 +0200

    roll all fives

--- scala/src/main/scala/bowlinggame.scala
+++ scala/src/main/scala/bowlinggame.scala
@@ -1,14 +1,21 @@
 class BowlingGame {
   def score(rolls: List[Int]):Int = {
-    rolls match {
-      case roll1::roll2::tail if(roll1+roll2==10) => 10 + firstRollOf(tail) + score(tail)
-      case roll1::roll2::tail => roll1 + roll2 + score(tail)
-      case head::tail => head
-      case Nil => 0
+    score(1, rolls)
+  }
+
+  def score(frame: Int, rolls: List[Int]):Int = {
+    frame match{
+      case f if f > 10 => 0
+      case _ => rolls match {
+        case roll1::roll2::tail if(roll1+roll2==10) => 10 + firstRollOf(tail) + score(frame+1,tail)
+        case roll1::roll2::tail => roll1 + roll2 + score(frame+1,tail)
+        case head::tail => head
+        case Nil => 0
+      }
     }
   }
 
-  def firstRollOf(rolls: List[Int]):Int = {
+  private def firstRollOf(rolls: List[Int]):Int = {
     rolls match {
       case Nil => 0
       case head::_tail => head
