bca7dd173ddf61b9359e7d1899ce0957e67bec81
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-17 17:34:12 +0200

    next

--- ruby/spec/bowling_game_spec.rb
+++ ruby/spec/bowling_game_spec.rb
@@ -7,12 +7,12 @@ describe BowlingGame do
   subject(:game) {BowlingGame.new}
 
   context 'a new game' do
-    its(:score){ is_expected.to eq 0 }
+    it{ is_expected.to score 0 }
   end
 
   context 'rolling 0' do
     before{ game.roll(0) }
-    its(:score){ is_expected.to eq 0 }
+    it{ is_expected.to score 0 }
   end
 
 end
