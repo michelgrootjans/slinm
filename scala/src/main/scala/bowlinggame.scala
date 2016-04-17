923feb3f013646bd52f0331a5dcb969ff40bf540
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-17 22:29:15 +0200

    next

--- scala/src/main/scala/bowlinggame.scala
+++ scala/src/main/scala/bowlinggame.scala
@@ -1,8 +1,17 @@
 class BowlingGame {
   def score(rolls: List[Int]):Int = {
     rolls match {
-      case Nil => 0
+      case roll1::roll2::tail if(roll1+roll2==10) => 10 + firstRollOf(tail) + score(tail)
+      case roll1::roll2::tail => roll1 + roll2 + score(tail)
       case head::tail => head
+      case Nil => 0
+    }
+  }
+
+  def firstRollOf(rolls: List[Int]):Int = {
+    rolls match {
+      case Nil => 0
+      case head::_tail => head
     }
   }
 }
