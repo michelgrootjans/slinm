ca75770e1237e69b5d37fd1083f32dda55162f6a
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-17 22:37:07 +0200

    next

--- scala/src/main/scala/bowlinggame.scala
+++ scala/src/main/scala/bowlinggame.scala
@@ -7,6 +7,7 @@ class BowlingGame {
     frame match{
       case f if f > 10 => 0
       case _ => rolls match {
+        case roll::tail if(roll==10) => 10 + firstRollOf(tail) + secondRollOf(tail) + score(frame+1,tail)
         case roll1::roll2::tail if(roll1+roll2==10) => 10 + firstRollOf(tail) + score(frame+1,tail)
         case roll1::roll2::tail => roll1 + roll2 + score(frame+1,tail)
         case head::tail => head
@@ -19,6 +20,13 @@ class BowlingGame {
     rolls match {
       case Nil => 0
       case head::_tail => head
+    }
+  }
+  
+  private def secondRollOf(rolls: List[Int]):Int = {
+    rolls match {
+      case Nil => 0
+      case _head::tail => firstRollOf(tail)
     }
   }
 }
