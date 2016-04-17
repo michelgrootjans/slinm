3e0c77e05f522b55b081aa829fe8436ee1c26f42
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-17 16:55:49 +0200

    next

--- csharp/BowlingGame.cs
+++ csharp/BowlingGame.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace BowlingGame {
   public class BowlingGame{
     public void Roll(int pins){
-      Score = pins;
+      Score += pins;
     }
     public int Score{get; private set;}
   }
