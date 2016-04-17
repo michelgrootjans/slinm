b422d66106b4cf32774eb8979faf1b0d668f34f4
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-17 16:56:37 +0200

    next

--- csharp/BowlingGameTests.cs
+++ csharp/BowlingGameTests.cs
@@ -40,5 +40,13 @@ namespace BowlingGame
       game.Roll(1);
       Assert.Equal(2, game.Score);
     }
+
+    [Fact]
+    public void Rolling_spare_2(){
+      game.Roll(4);
+      game.Roll(6);
+      game.Roll(2);
+      Assert.Equal(4+6+2 + 2, game.Score);
+    }
   }
 }
