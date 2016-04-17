5f00d36a34fe8ab7678f323a16ce9237894c42ef
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-17 17:27:03 +0200

    next

--- ruby/spec/bowling_game_spec.rb
+++ ruby/spec/bowling_game_spec.rb
@@ -3,7 +3,11 @@ require 'rspec/its'
 require 'support/score_matcher'
 
 describe BowlingGame do
+
+  let(:game) {BowlingGame.new}
+
   context 'a new game' do
-    it { expect(BowlingGame.new.score).to eq 0 }
+    it { expect(game.score).to eq 0 }
   end
+
 end
