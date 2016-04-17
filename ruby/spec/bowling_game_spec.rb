2aec606c60f1da776e6578b7b7968ab1b9b2f8c5
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-17 17:30:24 +0200

    next

--- ruby/spec/bowling_game_spec.rb
+++ ruby/spec/bowling_game_spec.rb
@@ -4,7 +4,7 @@ require 'support/score_matcher'
 
 describe BowlingGame do
 
-  let(:game) {BowlingGame.new}
+  subject(:game) {BowlingGame.new}
 
   context 'a new game' do
     it { expect(game.score).to eq 0 }
