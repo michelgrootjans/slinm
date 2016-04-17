1898d3ce407d5a046fc3ae995de54a93e418f3ce
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-17 21:43:07 +0200

    next

--- fsharp/bowlinggame.fs
+++ fsharp/bowlinggame.fs
@@ -1,8 +1,12 @@
 namespace BowlingGame
 module BowlingGame =
 
+  let first_roll_of rolls =
+    match rolls with
+      | head::tail -> head
+
   let rec score rolls =
     match rolls with
-      | roll1::roll2::tail when roll1+roll2=10 -> roll1 + roll2 + score(tail)
+      | roll1::roll2::tail when roll1+roll2=10 -> roll1 + roll2 + first_roll_of(tail) + score(tail)
       | head::tail -> head + score(tail)
       | _ -> 0
