c1089a8359c0d50f582925189287a80b45964f4d
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-17 21:40:51 +0200

    next

--- fsharp/bowlinggame.fs
+++ fsharp/bowlinggame.fs
@@ -3,5 +3,6 @@ module BowlingGame =
 
   let rec score rolls =
     match rolls with
+      | roll1::roll2::tail when roll1+roll2=10 -> roll1 + roll2 + score(tail)
       | head::tail -> head + score(tail)
       | _ -> 0
