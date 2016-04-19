759178a0ed5033c26dbde8890faaa936cc2d9aa5
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-19 09:12:17 +0200

    next

--- cloj/src/cloj/bowling.clj
+++ cloj/src/cloj/bowling.clj
@@ -1,10 +1,16 @@
 (ns cloj.bowling)
 
+(defn- spare? [rolls]
+  (= 10 (reduce + (take 2 rolls))))
+
+(defn- rest-rolls [rolls]
+  (drop 2 rolls))
+
 (defn- to-frames [rolls]
-  (lazy-seq (cons (if (= 10 (reduce + (take 2 rolls)))
+  (lazy-seq (cons (if (spare? rolls)
                     (take 3 rolls)
                     (take 2 rolls))
-                  (to-frames (drop 2 rolls)))))
+                  (to-frames (rest-rolls rolls)))))
 
 (defn score [rolls]
   (reduce + (map #(reduce + %)
