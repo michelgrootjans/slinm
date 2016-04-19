0df9efc048e60f01832e96036289e8497f91d9b1
Michel Grootjans <michel.grootjans@gmail.com>
2016-04-19 09:09:31 +0200

    next

--- cloj/src/cloj/bowling.clj
+++ cloj/src/cloj/bowling.clj
@@ -1,4 +1,11 @@
 (ns cloj.bowling)
 
+(defn- to-frames [rolls]
+  (lazy-seq (cons (if (= 10 (reduce + (take 2 rolls)))
+                    (take 3 rolls)
+                    (take 2 rolls))
+                  (to-frames (drop 2 rolls)))))
+
 (defn score [rolls]
-  (reduce + (take 20 rolls)))
+  (reduce + (map #(reduce + %)
+                 (take 10 (to-frames rolls)))))
